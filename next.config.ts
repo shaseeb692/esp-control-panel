e5dee0b9a1baf5cfad114427ee93211a831bab46
shaseeb692 <shaseeb692@gmail.com>
2026-08-29 12:55:09 +0500

    Add security headers

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,14 @@ const nextConfig: NextConfig = {
             key: "Referrer-Policy",
             value: "strict-origin-when-cross-origin",
           },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=(), geolocation=()",
+          },
         ],
       },
     ];
