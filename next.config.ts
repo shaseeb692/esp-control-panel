ea6ac18162fa97c46bc7617ca2ee39e447ff3075
shaseeb692 <shaseeb692@gmail.com>
2026-09-16 12:20:12 +0500

    Allow camera for QR device scanner

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: "Permissions-Policy",
-            value: "camera=(), microphone=(), geolocation=()",
+            value: "camera=(self), microphone=(), geolocation=()",
           },
         ],
       },
